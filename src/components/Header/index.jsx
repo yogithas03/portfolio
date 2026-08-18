7ee17025706e5fb107d4236c10479fc6dbe2f5dc
yogitha03 <yogithasoundararajan03@gmail.com>
2026-08-18 10:50:15 +0530

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-// import Headroom from "react-headroom";
+import Headroom from "react-headroom";
 import { ThemeContext } from "../../context/ThemeContext";
 import {
   greeting,
